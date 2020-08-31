461fa39788f9bec4ac059f9c3267d3d54a6649fe
toastyhatesthsea <toastyhatesthesea@gmail.com>
2020-08-31 16:25:49 -0700

    Added compareTo to binary search

--- src/BasicSearches/BinarySearch.java
+++ src/BasicSearches/BinarySearch.java
@@ -2,4 +2,32 @@ package BasicSearches;
 
 public class BinarySearch
 {
+
+
+    public static <T extends Comparable> int search(T[] a, int first, int size, int target)
+    {
+        int middle;
+
+        if (size <= 0)
+        {
+            return -1;
+        }
+        else
+        {
+            middle = first + size / 2;
+            if (a[middle].compareTo(target) == 0)
+            {
+                return middle;
+            } else if (a[middle].compareTo(target) < 0)
+            {
+                return search(a, first, size / 2, target);
+            }
+            else
+            {
+                return search(a, middle + 1, (size - 1) / 2, target);
+            }
+        }
+    }
+
+
 }
