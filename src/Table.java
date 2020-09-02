949ebe7cea4d4c1a6fae0b39297836849f3478a6
toastyhatesthsea <toastyhatesthesea@gmail.com>
2020-09-02 01:08:50 -0700

    Implemented several methods for Table class, including containsKey(K key)

--- src/Table.java
+++ src/Table.java
@@ -1,5 +1,7 @@
 import java.util.Objects;
 
+import static java.util.Objects.hash;
+
 public class Table <K, E>
 {
 
@@ -20,5 +22,66 @@ public class Table <K, E>
         hasBeenUsed = new boolean[sizeOfTable];
     }
 
+    public boolean containsKey(K key)
+    {
+        int answer = findIndex(key);
 
+        return answer != -1;
+    }
+
+    private int findIndex(K key)
+    {
+        int count = 0;
+        int i = hash(key);
+
+        while ((count < data.length) && (hasBeenUsed[i]))
+        {
+            if (key.equals(keys[i]))
+            {
+                return i;
+            }
+            count++;
+            i = nextIndex(i);
+        }
+
+        return -1;
+    }
+
+    private int hash(Object key)
+    {
+        return Math.abs(key.hashCode()) % data.length;
+    }
+
+    /**
+     * Just circles back to the beginning of the array when index reaches the end
+     * @param i int
+     * @return int
+     */
+    private int nextIndex(int i)
+    {
+        if (i + 1 == data.length)
+        {
+            return 0;
+        }
+        else
+        {
+            return i + 1;
+        }
+    }
+
+}
+
+class TableTester
+{
+    public static void main(String[] sdfsdfsdfa)
+    {
+        int sizeOfTable = 50;
+        Table<String, String> thing = new Table<>(50);
+
+        String aString = "I love it";
+
+        int total = Math.abs(aString.hashCode()) % sizeOfTable;
+
+        total = 101 % sizeOfTable;
+    }
 }
